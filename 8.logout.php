a9c8fde14a05df9206796a7c04f9479cfef87cce
vivian313fan <74036598+vivian313fan@users.noreply.github.com>
2020-12-27 12:10:25 +0800

    Update 8.logout.php

--- 8.logout.php
+++ 8.logout.php
@@ -1,6 +1,6 @@
 <?php
 
-session_start(); // 儲存 session 資料
+session_start(); //啟動Session的使用
 unset($_SESSION["id"]); //將session清空
 echo '登出中......';
 echo '<meta http-equiv=REFRESH CONTENT=2;url=login.html>'; //上面例子會讓網頁在載入 2 秒後，自動跳轉到login.html
