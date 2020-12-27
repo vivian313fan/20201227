a79dd645da2d70409eab8e2d0d5df6f686b5235a
vivian313fan <74036598+vivian313fan@users.noreply.github.com>
2020-12-27 12:05:48 +0800

    Update 6.login.php

--- 6.login.php
+++ 6.login.php
@@ -1,12 +1,12 @@
 <?php
-    error_reporting(0);
+    error_reporting(0); //報告執行時錯誤
     
-    $conn = mysqli_connect("localhost","root","", "mydb");
+    $conn = mysqli_connect("localhost","root","", "mydb");  //建立資料庫連線，如果$conn=false代表連結不成功
     if (mysqli_connect_error($conn))
         die("資料庫連線錯誤");
 
-    mysqli_set_charset($conn, "utf8");
-    $result=mysqli_query($conn, "select * from user");
+    mysqli_set_charset($conn, "utf8"); //設置字符集
+    $result=mysqli_query($conn, "select * from user"); //查詢
     
     $login=FALSE;
     while ($row=mysqli_fetch_array($result)) {
@@ -16,16 +16,16 @@
     
     if  (!$_POST["id"] || !$_POST["pwd"]){
            echo "請輸入帳號/密碼"; 
-           echo "<meta http-equiv='refresh' content='3;url=login.html''>";              
+           echo "<meta http-equiv='refresh' content='3;url=login.html''>"; //上面例子會讓網頁在載入 3 秒後，自動跳轉到login.html              
     }
     elseif ($login==TRUE){
-      session_start();
+      session_start();  // 儲存 session 資料
       $_SESSION["id"] = $_POST['id'];
       echo "歡迎登入";    
-      echo "<meta http-equiv='refresh' content='0;url=bulletin.php''>";   
+      echo "<meta http-equiv='refresh' content='0;url=bulletin.php''>";   //上面例子會讓網頁在載入 0秒後，自動跳轉到bulletin.php 
     }
     else{
       echo "帳號密碼錯誤";
-      echo "<meta http-equiv='refresh' content='3;url=login.html''>";          
+      echo "<meta http-equiv='refresh' content='3;url=login.html''>";    //上面例子會讓網頁在載入 3 秒後，自動跳轉到login.html       
     }
 ?>
