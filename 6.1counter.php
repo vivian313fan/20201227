9961eb022a1699e485c2228b1c78739c500f9784
vivian313fan <74036598+vivian313fan@users.noreply.github.com>
2020-12-27 12:13:07 +0800

    Update 6.1counter.php

--- 6.1counter.php
+++ 6.1counter.php
@@ -1,8 +1,8 @@
 <?php
-    error_reporting(0);
-    session_start();
-    if (!isset($_SESSION["counter1"])){
-        $_SESSION["counter1"]=1;
+    error_reporting(0); //報告執行時錯誤
+    session_start();   //啟動Session的使用
+    if (!isset($_SESSION["counter1"])){  //檢查Session值是否存在
+        $_SESSION["counter1"]=1; //設定Session值
     }else{
         $_SESSION["counter1"]++;
     }
