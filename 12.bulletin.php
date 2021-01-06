74adc026857c32b06a890aaec5f1e31ebc8fa67b
vivian313fan <74036598+vivian313fan@users.noreply.github.com>
2021-01-06 16:55:49 +0800

    Update 12.bulletin.php

--- 12.bulletin.php
+++ 12.bulletin.php
@@ -6,7 +6,7 @@
         echo "<meta http-equiv='refresh' content='3; url=login.html'>";    //上面例子會讓網頁在載入 3 秒後，自動跳轉到login.html
     }else{
         echo "歡迎 {$_SESSION['id']} 登入 [<a href=logout.php>登出</a>]<p>[<a href=bulletin_add_form.php>新增佈告</a>]<p>";
-        $conn=mysqli_connect("localhost","root","", "mydb");
+        $conn=mysqli_connect("localhost","root","", "mydb");    //建立資料庫連線，如果$conn=false代表連結不成功
         $result=mysqli_query($conn, "select * from bulletin");
         echo "<table border=2><tr><td>佈告操作</td><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
         while ($row=mysqli_fetch_array($result)){
