b878b1cb21e8c08a2b042ae1f51611f23e17ae35
vivian313fan <74036598+vivian313fan@users.noreply.github.com>
2021-01-10 20:50:47 +0800

    Update 10.1delete.php

--- 10.1delete.php
+++ 10.1delete.php
@@ -1,6 +1,6 @@
 <?php
-    error_reporting();
-    $conn=mysqli_connect("localhost","root","","mydb");
+    error_reporting(); //用來設定錯誤級別並返回當前級別的
+    $conn=mysqli_connect("localhost","root","","mydb"); //建立資料庫連線，如果$conn=false代表連結不成功
     // delete from bulletin where bid=???
     $sql="delete from bulletin where bid={$_GET[bid]}";
     //echo $sql;
@@ -8,6 +8,6 @@
         echo "刪除錯誤";
     else{
         echo "刪除成功；回前頁中...";
-        echo "<meta http-equiv='refresh' content='3;url=bulletin.php'>"; 
+        echo "<meta http-equiv='refresh' content='3;url=bulletin.php'>"; //上面例子會讓網頁在載入 3秒後，自動跳轉到bulletin.php 
     }
 ?>
