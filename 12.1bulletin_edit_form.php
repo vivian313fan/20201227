0d5899f5f08a44ae47f3e5b36f87352503d5ff5b
vivian313fan <74036598+vivian313fan@users.noreply.github.com>
2021-01-10 22:03:37 +0800

    Update 12.1bulletin_edit_form.php

--- 12.1bulletin_edit_form.php
+++ 12.1bulletin_edit_form.php
@@ -12,7 +12,7 @@ echo "
 <html>
 <head><title>修改佈告</title></head> 
 <body>   <!--用來呈現網頁的主要內容-->
-  <h2>修改佈告</h2> H2代表副標題
+  <h2>修改佈告</h2> <!--h2代表副標題-->
   <form action='bulletin_edit.php' method='post'> <!--當按下送出表單之後，會將資料傳送到 bulletin_edit.php 這程式。-->
     <input type=hidden name=bid value=$row[bid]>
     佈告標題：<input  type='text' name='title' size=200 value='$row[title]'><p>      
@@ -30,8 +30,8 @@ echo "
     if ($row['type']==3) echo "checked ";
     echo "type='radio'>企業徵才<p>      
     發佈時間：<input type=date name='time' value=$row[time]><p>      
-    <input type=submit> //這是最常見的提交方式
-  </form>  //用來建立一個 HTML 表單
+    <input type=submit> <!--這是最常見的提交方式-->
+  </form>  <!--用來建立一個 HTML 表單-->
 </body>
 </html>
 "; 
