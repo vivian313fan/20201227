cf41f69cfed0278d333374d6cb4f510990c373d5
vivian313fan <74036598+vivian313fan@users.noreply.github.com>
2021-01-10 20:43:00 +0800

    Update 11.1bulletin_add_form.php

--- 11.1bulletin_add_form.php
+++ 11.1bulletin_add_form.php
@@ -5,7 +5,7 @@
     <form action="bulletin_add.php" method="post">	 //表單FROM是 action= 'bulletin_edit.php'，這代表當網友按下送出表單之後，會將資料傳送到 bulletin_add.php 這程式
     佈告內容：<p>
 	<textarea rows="20" cols="100" name="content"></textarea>  
-	     //打在<TITLE></TITLE>這裡面的文字會出現在瀏覽器視窗最上頭藍色部份裡，當作一篇網頁的主題。
+	  // TEXTAREA在表單中，建立一個可以輸入多行文字的輸入框。rows設定輸入框的高度、cols設定輸入框的寬度、name: 聲明欄位名稱   
     <p>
     佈告類型：<input type="radio" name="type" value="1" checked >系上公告  //<input> 的 type 屬性是建立不同類型的表單元件
               <input type="radio" name="type" value="2">招生訊息 
@@ -16,4 +16,4 @@
   </form>
 </body>
 </html> 
-//TEXTAREA在表單中，建立一個可以輸入多行文字的輸入框。rows設定輸入框的高度、cols設定輸入框的寬度、name: 聲明欄位名稱
+
