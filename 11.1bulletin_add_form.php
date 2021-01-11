2e1fb8a76a28dd7d7fe047faf9effed25c00b3c7
vivian313fan <74036598+vivian313fan@users.noreply.github.com>
2021-01-11 15:50:40 +0800

    Update 11.1bulletin_add_form.php

--- 11.1bulletin_add_form.php
+++ 11.1bulletin_add_form.php
@@ -1,13 +1,13 @@
 <html>
 <title>新增佈告</title>  //打在<TITLE></TITLE>這裡面的文字會出現在瀏覽器視窗最上頭藍色部份裡，當作一篇網頁的主題。
-	//第七行 表單是action=bulletin_add.php當按下送出表單之後，會將資料傳送到bulletin_add.php這程式。
-	 //第十行 TEXTAREA在表單中，建立一個可以輸入多行文字的輸入框。rows設定輸入框的高度、cols設定輸入框的寬度、name:聲明欄位名稱。	
+	
+		
 <body>
     <h2>新增佈告</h2>
-    <form action="bulletin_add.php" method="post">  
+    <form action="bulletin_add.php" method="post">  <!-- 表單是action=bulletin_add.php當按下送出表單之後，會將資料傳送到bulletin_add.php這程式。-->
     標題：<input  type="text" name="title" size="100"><p>
     佈告內容：<p>
-	<textarea rows="20" cols="100" name="content"></textarea> 
+	<textarea rows="20" cols="100" name="content"></textarea> <!--TEXTAREA在表單中，建立一個可以輸入多行文字的輸入框。rows設定輸入框的高度、cols設定輸入框的寬度、name:聲明欄位名稱。-->
     <p>
     佈告類型：<input type="radio" name="type" value="1" checked >系上公告 
               <input type="radio" name="type" value="2">招生訊息 
